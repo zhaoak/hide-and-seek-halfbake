5b173e7693edf4965b4f0261a88fbee73415525b
Allie Zhao <allie.k.zhao@gmail.com>
2022-10-31 12:24:21 -0700

    finished rubric requirements, added reset stats button, probably should have committed before this

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const shedButton = document.getElementById('shed-button');
 const treeButton = document.getElementById('tree-button');
 const boulderButton = document.getElementById('boulder-button');
+const resetStatsButton = document.getElementById('resetStatsButton');
 
 const shedContainer = document.getElementById('shed-container');
 const treeContainer = document.getElementById('tree-container');
@@ -35,11 +36,39 @@ boulderButton.addEventListener('click', () => {
     handleGuess(answer, 'boulder');
 });
 
+resetStatsButton.addEventListener('click', () => {
+    correctGuesses = 0;
+    totalGuesses = 0;
+    totalEl.textContent = 0;
+    lossesEl.textContent = 0;
+    winsEl.textContent = 0;
+});
+
 function handleGuess(correctSpot, userGuess) {
     // reset the styles
+    shedContainer.classList.remove('face');
+    treeContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
+
     // then increment the guesses
+    totalGuesses++;
+
     // then grab the appropriate container element for the correct guess from the DOM
     // then add the face class to that element so that the face shows up
+    if (correctSpot === 'shed') {
+        shedContainer.classList.toggle('face');
+    } else if (correctSpot === 'tree') {
+        treeContainer.classList.toggle('face');
+    } else {
+        boulderContainer.classList.toggle('face');
+    }
+
     // then if the user guess is correct, increment the correct guesses
+    if (userGuess === correctSpot) {
+        correctGuesses++;
+    }
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
+    totalEl.textContent = totalGuesses;
+    lossesEl.textContent = totalGuesses - correctGuesses;
+    winsEl.textContent = correctGuesses;
 }
