ba976463daf3d51f986bccb9814303461830a9c5
Allie Zhao <allie.k.zhao@gmail.com>
2022-10-31 13:08:17 -0700

    added try again button user must click to reset game state

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const shedButton = document.getElementById('shed-button');
 const treeButton = document.getElementById('tree-button');
 const boulderButton = document.getElementById('boulder-button');
 const resetStatsButton = document.getElementById('resetStatsButton');
+const tryAgainButton = document.getElementById('tryAgainButton');
 
 const shedContainer = document.getElementById('shed-container');
 const treeContainer = document.getElementById('tree-container');
@@ -14,6 +15,7 @@ const winsEl = document.getElementById('wins');
 
 // initialize state
 const hidingPlaces = ['tree', 'shed', 'boulder'];
+tryAgainButton.disabled = true;
 
 let correctGuesses = 0;
 let totalGuesses = 0;
@@ -42,13 +44,38 @@ resetStatsButton.addEventListener('click', () => {
     totalEl.textContent = 0;
     lossesEl.textContent = 0;
     winsEl.textContent = 0;
-});
 
-function handleGuess(correctSpot, userGuess) {
-    // reset the styles
+    // also should reset game state to initial state
+    tryAgainButton.disabled = true;
+    shedButton.disabled = false;
+    treeButton.disabled = false;
+    boulderButton.disabled = false;
     shedContainer.classList.remove('face');
     treeContainer.classList.remove('face');
     boulderContainer.classList.remove('face');
+});
+
+tryAgainButton.addEventListener('click', () => {
+    // hide emoji showing where hiding spot was
+    shedContainer.classList.remove('face');
+    treeContainer.classList.remove('face');
+    boulderContainer.classList.remove('face');
+
+    // enable buttons for picking hiding places again
+    shedButton.disabled = false;
+    treeButton.disabled = false;
+    boulderButton.disabled = false;
+
+    // disable try again button
+    tryAgainButton.disabled = true;
+});
+
+function handleGuess(correctSpot, userGuess) {
+    // disable the hiding place buttons, enable try again button
+    shedButton.disabled = true;
+    treeButton.disabled = true;
+    boulderButton.disabled = true;
+    tryAgainButton.disabled = false;
 
     // then increment the guesses
     totalGuesses++;
